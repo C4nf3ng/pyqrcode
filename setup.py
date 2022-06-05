8eedbcffc1e33f7466fdc140fad241065c586184
C4nf3ng <root@macrodef.com>
2022-06-05 12:41:42 +0800

    add project url and description

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 from distutils.extension import Extension
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
 ext_modules = [
@@ -19,4 +21,8 @@ packages=find_packages(),
 author='C4nf3ng',
 license='GNU General Public License v3.0',
 description='A python binding for https://github.com/ricmoo/QRCode with some modification',
+url = 'https://github.com/C4nf3ng/pyqrcode',
+long_description=long_description,
+long_description_content_type="text/markdown",
+
 )
