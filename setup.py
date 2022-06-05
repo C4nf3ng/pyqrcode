422798370bcb54738d2a526ad4264192b954407a
c4nf3ng <root@macrodef.com>
2022-06-05 12:05:01 +0800

    optimize setup.py

--- setup.py
+++ setup.py
@@ -17,5 +17,6 @@ install_requires=['cython'],
 version='0.0.1',
 packages=find_packages(),
 author='C4nf3ng',
+license='GNU General Public License v3.0',
 description='A python binding for https://github.com/ricmoo/QRCode with some modification',
 )
